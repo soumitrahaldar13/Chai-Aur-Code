d551f20a49f0e332a814f7db9d10b6c88303f97e
soumitrahaldar13 <soumitrahalder13@gmail.com>
2024-11-21 22:23:45 +0530

    hhhhhg

--- comments/script.js
+++ comments/script.js
@@ -1,3 +1,22 @@
 console.log("hello world");
 //this is a comment
 /*this is a comment*/
+//Aritimetic operator
+// let a = 6;
+// let b = 2;
+// console.log("a=" ,a ,"& b=",b);
+// console.log("a+b=" ,a+b);
+// console.log("a-b=" ,a-b);
+// console.log("a*b=", a*b);
+// console.log("a/b=" ,a/b);
+// console.log("a%b=" ,a%b);
+// console.log("a**b=" ,a**b);
+//unary Operator
+
+let a = 6;
+let b = 2;
+console.log("a=" ,a ,"& b=",b);
+//a = a+1;
+//a++;
+//a--;
+console.log("a=",a);

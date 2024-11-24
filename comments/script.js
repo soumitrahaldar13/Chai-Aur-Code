a3fa30f83487ff545cfc29232a796c888be5d19c
soumitrahaldar13 <soumitrahalder13@gmail.com>
2024-11-24 20:00:20 +0530

    if else

--- comments/script.js
+++ comments/script.js
@@ -19,4 +19,23 @@ console.log("a=" ,a ,"& b=",b);
 //a = a+1;
 //a++;
 //a--;
-console.log("a=",a);
+//console.log("a=",a);
+// a +=5
+// console.log(a);//a=6+5
+// start if else conditional statements
+//example 1
+// let age =18;
+// if (age>=18) {
+//     console.log("age=",age,"(vote)");
+// }else{
+//     console.log("age=",age,"(not vote)");
+// }
+//example 2 odd &event
+let num =11;
+if(num % 2 ===0){
+    console.log(num,"is event");
+}else{
+    console.log(num ,"is odd");
+}
+
+// end if else conditional statements
